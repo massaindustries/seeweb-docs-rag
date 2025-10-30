1cbb29dcc2fac1a8743bfb3909cb5acc38a5d966
massaindustries <francescomassa06@gmail.com>
2025-10-31 00:48:09 +0100

    latenightrag01
    Signed-off-by: massaindustries <francescomassa06@gmail.com>

--- query_data.py
+++ query_data.py
@@ -1,7 +1,7 @@
 import argparse
 from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_ollama import Ollama
+from langchain_ollama import ChatOllama
 
 from get_embedding_function import get_embedding_function
 
@@ -36,12 +36,15 @@ def query_rag(query_text: str):
     results = db.similarity_search_with_score(query_text, k=5)
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    
+    # Create chain
+    model = ChatOllama(model="llama3.3:70b")
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
-
-    model = Ollama(model="llama3.3:70b")
-    response_text = model.invoke(prompt)
+    chain = prompt_template | model
+    
+    # Invoke chain
+    response = chain.invoke({"context": context_text, "question": query_text})
+    response_text = response.content
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
