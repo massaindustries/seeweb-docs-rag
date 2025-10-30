156080929e8ad11fbfbbb9a22ec44543f5a3fc48
massaindustries <francescomassa06@gmail.com>
2025-10-30 23:49:08 +0100

    newlang01
    Signed-off-by: massaindustries <francescomassa06@gmail.com>

--- query_data.py
+++ query_data.py
@@ -1,7 +1,7 @@
 import argparse
-from langchain_community.vectorstores.chroma import Chroma
+from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.llms.ollama import Ollama
+from langchain_ollama import Ollama
 
 from get_embedding_function import get_embedding_function
 
